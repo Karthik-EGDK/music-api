de2d9fe7f84247da30f5f85bf0322c916c07d69a
Lavisha Andri Serrao <laase@eg.dk>
2024-04-08 16:42:23 +0530

    added new songs

--- index.js
+++ index.js
@@ -44,6 +44,58 @@ app.post("/music",(req,res)=>{
                              musicTitle:"Midnight"
         }]})
         }
+        else if(name=="love dose"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573171/api%20musics/lavisha/Love_Dose_2.0_Remix_-_DJ_Shadow_Dubai_64_PagalWorld.com.cm_usxyxh.mp3",
+                             musicTitle:"Love dose 2.0"
+        }]})
+        }
+        else if(name=="kahani suno"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573170/api%20musics/lavisha/Kahani_Suno_64_PagalWorld.com.cm_viglee.mp3",
+                             musicTitle:"Kahani Suno"
+        }]})
+        }
+
+        else if(name=="choli ke peeche"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573169/api%20musics/lavisha/Choli_Ke_Peeche_64_PagalWorld.com.cm_rfqedc.mp3",
+                             musicTitle:"Choli ke peeche"
+        }]})
+        }
+        else if(name=="kesariya"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573127/api%20musics/lavisha/Kesariya_64_PagalWorld.com.cm_ni2t0b.mp3",
+                             musicTitle:"Kesariya"
+        }]})
+        }
+        else if(name=="gulabi sadi"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573163/api%20musics/lavisha/Gulabi_Sadi_Ani_Lali_Lal_Lal_64_PagalWorld.com.cm_ooqtsi.mp3",
+                             musicTitle:"Gulabi sadi"
+        }]})
+        }
+        else if(name=="o mahi"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573163/api%20musics/lavisha/O_Mahi_O_Mahi_64_PagalWorld.com.cm_zwghuv.mp3",
+                             musicTitle:"O mahi"
+        }]})
+        }
+        else if(name=="teri baaton mein aisa ulja jiya"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573165/api%20musics/lavisha/Teri_Baaton_Mein_Aisa_Uljha_Jiya_64_PagalWorld.com.cm_deimrv.mp3",
+                             musicTitle:"Teri baaton mein aisa ulja jiya"
+        }]})
+        }
+        else if(name=="naina"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573165/api%20musics/lavisha/_Naina_64_PagalWorld.com.cm_rnifqg.mp3",
+                             musicTitle:"Naina"
+        }]})
+        }
+        else if(name=="chaleya"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573166/api%20musics/lavisha/Chaleya_64_PagalWorld.com.cm_c6nvsb.mp3",
+                             musicTitle:"Chaleya"
+        }]})
+        }
+        else if(name=="yimmy yimmy"){
+            return res.json({success:true,songs:[{musicfile:"https://res.cloudinary.com/dfjqihjz4/video/upload/v1712573167/api%20musics/lavisha/Yimmy_Yimmy_64_PagalWorld.com.cm_qy1k3y.mp3",
+                             musicTitle:"Yimmy yimmy"
+        }]})
+        }
+        
 
 
         return res.status(404).json({success:false,error:"song not found"})
